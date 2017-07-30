50fd2b96d8913d1daadbcc7ce1ad8d903f3762ca
purple-beastie <purple.beastie@tuta.io>
2017-07-30 23:11:52 +0000

    Create e6ExtendedBlacklist.meta.js

---
+++ e6ExtendedBlacklist.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         e6ExtendedBlacklist
+// @namespace    prplbst
+// @version      0.1.0
+// @description  Allows you to have a blacklist longer than 3900 characters
+// @author       purple.beastie
+// @updateURL    https://raw.githubusercontent.com/purple-beastie/e6ExtendedBlacklist/master/e6ExtendedBlacklist.meta.js
+// @match        https://e621.net/*
+// @match        https://e926.net/*
+// @grant        none
+// ==/UserScript==
